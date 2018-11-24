063eef6f76024821016203a005610c20468807f0
Daniel Marroquin <Daniel@Daniels-MacBook-Pro.local>
2018-11-24 22:34:25 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,6 +22,7 @@ suit: "diamonds",
 cardImage: "images/king-of-diamonds.png"
 }
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -33,14 +34,25 @@ var checkForMatch = function(){
 };
 
 var flipCard = function(cardId){
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
+cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 checkForMatch();
 }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		document.getElementById('game-board').appendChild(cardElement);
+		cardElement.addEventListener('click', flipCard);
+	}
+};
+
+createBoard();
