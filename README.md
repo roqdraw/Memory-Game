acdb2d7286bf6d332a6b184db629d02b2a0d00b8
roqdraw <roqdraw@gmail.com>
2019-04-28 21:47:01 +1000

    updated size responsiveness and cleaned up js code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # wdi-fundamentals-memorygame
+
+Exercise on the basics of HTML, CSS and JS.
+Part of the General Assembly Pre-Work.
+
+Play it here: link.
--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,26 @@
-console.log("Up and running!");
+console.log("Let's play.");
 
 var cards = [
-{
-rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
-},
-{
-rank: "queen",
-suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png"
-},
-{
-rank: "king",
-suit: "hearts",
-cardImage: "images/king-of-hearts.png"
-},
-{
-rank: "king",
-suit: "diamonds",
-cardImage: "images/king-of-diamonds.png"
-}
+	{
+	rank: "Queen",
+	suit: "Hearts",
+	cardImage: "memory_game/images/queen-of-hearts.png"
+	},
+	{
+	rank: "Queen",
+	suit: "Diamonds",
+	cardImage: "memory_game/images/queen-of-diamonds.png"
+	},
+	{
+	rank: "King",
+	suit: "Hearts",
+	cardImage: "memory_game/images/king-of-hearts.png"
+	},
+	{
+	rank: "King",
+	suit: "Diamonds",
+	cardImage: "memory_game/images/king-of-diamonds.png"
+	}
 ];
 
 var cardsInPlay = [];
@@ -33,22 +33,21 @@ var checkForMatch = function(){
 	}
 };
 
-var flipCard = function(cardId){
-var cardId = this.getAttribute('data-id');
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-cardsInPlay.push(cards[cardId].rank);
-this.setAttribute('src', cards[cardId].cardImage);
-if (cardsInPlay.length === 2) {
-checkForMatch();
-}
+var flipCard = function(cardId) {
+	var cardId = this.getAttribute('data-id');
+	console.log(`User flipped ${cards[cardId].rank} of ${cards[cardId].suit}`);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 };
 
-var createBoard = function () {
+var createBoard = function() {
 	for (var i = 0; i < cards.length; i++){
 		var cardElement = document.createElement('img');
-		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('src', "memory_game/images/back.png");
 		cardElement.setAttribute('data-id', i);
 		document.getElementById('game-board').appendChild(cardElement);
 		cardElement.addEventListener('click', flipCard);
